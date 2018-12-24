132de7f43f6b5851c7ab7fe0f6c944757bd945a8
unknown <jafarcees@gmail.com>
2018-12-25 00:54:17 +0530

    updated the landing page with new UI

--- lib/landing.dart
+++ lib/landing.dart
@@ -1,14 +1,177 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Landing  extends StatefulWidget {
+class Landing extends StatefulWidget {
   _LandingState createState() => _LandingState();
 }
 
-class _LandingState extends State<Landing > {
+class _LandingState extends State<Landing> {
+  bool _isUpdating = false;
+  int _currentValue = null;
+
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('Hello World'),
+      height: double.infinity,
+      width: double.infinity,
+      // color: Colors.teal,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[_tokenContainer(), _intervalIndicators()],
+      ),
+    );
+  }
+
+  Widget _tokenContainer() {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            // color: Colors.red,
+            child: Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                // Text('10', style: Theme.of(context).textTheme.display4),
+                _firestoreToken(),
+                Text(
+                  'Current Token Number',
+                  style: Theme.of(context).textTheme.subtitle,
+                )
+              ],
+            )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                iconSize: 40.0,
+                icon: Icon(
+                  Icons.navigate_before,
+                  color: Colors.orange,
+                ),
+                disabledColor: Colors.grey,
+                onPressed: _isUpdating
+                    ? null
+                    : () {
+                        _updateToken(-1);
+                      },
+              ),
+              SizedBox(
+                width: 40.0,
+              ),
+              IconButton(
+                icon: Icon(Icons.restore),
+                onPressed: () {},
+              ),
+              SizedBox(
+                width: 40.0,
+              ),
+              IconButton(
+                iconSize: 40.0,
+                icon: Icon(
+                  Icons.navigate_next,
+                  color: Colors.orange,
+                ),
+                disabledColor: Colors.red,
+                onPressed: _isUpdating
+                    ? null
+                    : () {
+                        _updateToken(1);
+                      },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  void _updateToken(int value) async {
+    setState(() {
+      print('inside trasaction');
+      _isUpdating = true;
+    });
+    final DocumentReference postRef =
+        Firestore.instance.collection('token').document('tokenNumber');
+    Firestore.instance.runTransaction((Transaction tx) async {
+      print('inside transaction block');
+      DocumentSnapshot postSnapshot = await tx.get(postRef);
+      if (postSnapshot.exists) {
+        try {
+          await tx.update(postRef,
+              <String, dynamic>{'token': postSnapshot.data['token'] + value});
+          setState(() {
+            print('setting the updating flag');
+            _isUpdating = false;
+          });
+        } catch (e) {
+          setState(() {
+            print('some error happened');
+            _isUpdating = false;
+          });
+        }
+      }
+    });
+  }
+
+  Widget _firestoreToken() {
+    return StreamBuilder<DocumentSnapshot>(
+        stream: Firestore.instance
+            .collection('token')
+            .document('tokenNumber')
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text('Loading...');
+            default:
+              return Text(
+                snapshot.data['token'].toString(),
+                style: Theme.of(context).textTheme.display4,
+              );
+          }
+        });
+  }
+
+  Widget _intervalIndicators() {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Material(
+        elevation: 4.0,
+        borderRadius: BorderRadius.circular(10.0),
+        child: Container(
+          height: 60.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              _circularAvartarIconButton(
+                  Colors.blueAccent, Icons.free_breakfast),
+              _circularAvartarIconButton(Colors.greenAccent, Icons.event_busy),
+              _circularAvartarIconButton(Colors.orange, Icons.restaurant),
+              _circularAvartarIconButton(Colors.pinkAccent, Icons.child_care)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _circularAvartarIconButton(Color backColor, IconData btnIcon) {
+    return CircleAvatar(
+      backgroundColor: backColor,
+      child: IconButton(
+        icon: Icon(
+          btnIcon,
+          color: Colors.white,
+        ),
+        disabledColor: Colors.red,
+        onPressed: null,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:token_system/landing.dart';
+import 'package:token_system/notes.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,97 +30,183 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _isUpdating = false;
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _incrementToken() {
-    setState(() {
-      _isUpdating = true;
-      final DocumentReference postRef =
-          Firestore.instance.document('token/tokenNumber');
-      Firestore.instance.runTransaction((Transaction tx) async {
-        DocumentSnapshot postSnapshot = await tx.get(postRef);
-        if (postSnapshot.exists) {
-          print('running transaction');
-          await tx.update(postRef,
-              <String, dynamic>{'token': postSnapshot.data['token'] + 1});
-          setState(() {
-            // Future.delayed(const Duration(microseconds: 500));
-            _isUpdating = false;
-          });
-        }
-      });
-    });
-  }
-
-  void _decrementToken() {}
+  int _currentIndex = 0;
+  List<Widget> _widgets = [Landing(), Notes()];
 
   @override
   Widget build(BuildContext context) {
-    print(_isUpdating);
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Current Token Number is:',
-              style: Theme.of(context).textTheme.overline,
-            ),
-            Container(
-              height: 150.0,
-              child:
-                  _isUpdating ? CircularProgressIndicator() : _tockenNumber(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                    icon: Icon(Icons.arrow_drop_down),
-                    tooltip: 'Decrement the token',
-                    iconSize: 50.0,
-                    onPressed: _decrementToken),
-                SizedBox(
-                  width: 30.0,
-                ),
-                IconButton(
-                  icon: Icon(Icons.arrow_drop_up),
-                  tooltip: 'Increment the token',
-                  color: Colors.red,
-                  iconSize: 50.0,
-                  onPressed: _incrementToken,
-                )
-              ],
-            )
-          ],
-        ),
+      body: SafeArea(
+        child: _widgets[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.multiline_chart),
+            title: Text('token'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            title: Text('info'),
+          )
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
       ),
     );
   }
 
-  Widget _tockenNumber() {
-    return StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance
-            .collection('token')
-            .document('tokenNumber')
-            .snapshots(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return new Text('Loading...');
-            default:
-              return Text(
-                snapshot.data['token'].toString(),
-                style: Theme.of(context).textTheme.display4,
-              );
-          }
-        });
-  }
+  // return StreamBuilder<DocumentSnapshot>(
+  //     stream: Firestore.instance
+  //         .collection('token')
+  //         .document('tokenNumber')
+  //         .snapshots(),
+  //     builder:
+  //         (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+  //       if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+  //       switch (snapshot.connectionState) {
+  //         case ConnectionState.waiting:
+  //           return new Text('Loading...');
+  //         default:
+  //           return Text(
+  //             snapshot.data['token'].toString(),
+  //             style: Theme.of(context).textTheme.display4,
+  //           );
+  //       }
+  //     });
+  // }
 }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _currentIndex = 0;
+//   int _counter = 0;
+//   bool _isUpdating = false;
+//   List<Widget> _widgets = [Landing(), Notes()];
+
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+//   void _incrementToken() {
+//     setState(() {
+//       _isUpdating = true;
+//       final DocumentReference postRef =
+//           Firestore.instance.document('token/tokenNumber');
+//       Firestore.instance.runTransaction((Transaction tx) async {
+//         DocumentSnapshot postSnapshot = await tx.get(postRef);
+//         if (postSnapshot.exists) {
+//           print('running transaction');
+//           await tx.update(postRef,
+//               <String, dynamic>{'token': postSnapshot.data['token'] + 1});
+//           setState(() {
+//             // Future.delayed(const Duration(microseconds: 500));
+//             _isUpdating = false;
+//           });
+//         }
+//       });
+//     });
+//   }
+
+//   void _decrementToken() {}
+
+//   @override
+//   Widget build(BuildContext context) {
+//     print(_isUpdating);
+//     return Scaffold(
+//       body: SafeArea(
+//         child: _widgets[_currentIndex],
+//       ),
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: _currentIndex,
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.alarm),
+//             title: Text('token'),
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(Icons.arrow_back),
+//             title: Text('info'),
+//           )
+//         ],
+//         onTap: (index) {
+//           setState(() {
+//             _currentIndex = index;
+//           });
+//         },
+//       ),
+//     );
+//   }
+
+//   Widget _tockenNumber() {
+//     return StreamBuilder<DocumentSnapshot>(
+//         stream: Firestore.instance
+//             .collection('token')
+//             .document('tokenNumber')
+//             .snapshots(),
+//         builder:
+//             (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+//           if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+//           switch (snapshot.connectionState) {
+//             case ConnectionState.waiting:
+//               return new Text('Loading...');
+//             default:
+//               return Text(
+//                 snapshot.data['token'].toString(),
+//                 style: Theme.of(context).textTheme.display4,
+//               );
+//           }
+//         });
+//   }
+// }
+
+// Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'Current Token Number is:',
+//               style: Theme.of(context).textTheme.overline,
+//             ),
+//             Container(
+//               height: 150.0,
+//               child:
+//                   _isUpdating ? CircularProgressIndicator() : _tockenNumber(),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: <Widget>[
+//                 IconButton(
+//                     icon: Icon(Icons.arrow_drop_down),
+//                     tooltip: 'Decrement the token',
+//                     iconSize: 50.0,
+//                     onPressed: _decrementToken),
+//                 SizedBox(
+//                   width: 30.0,
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.arrow_drop_up),
+//                   tooltip: 'Increment the token',
+//                   color: Colors.red,
+//                   iconSize: 50.0,
+//                   onPressed: _incrementToken,
+//                 )
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
